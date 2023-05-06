fc9314a97283af1304a2743e2f498a559156b043
CSCStudent <robert.gilanyi@frontrange.edu>
2023-05-05 19:00:07 -0600

    5/5 corrected indentations

--- WolfCasino/Casino.cpp
+++ WolfCasino/Casino.cpp
@@ -16,10 +16,10 @@ namespace CSC2034 {
 			game = TicTacToeGame::makeGame(); // TicTacToeGame::makeGame();
 
 		else if (gameName == "Othello")
-            		game = othello::makeGame();
+            game = othello::makeGame();
 
-    		else if (gameName == "TicTacToeGame")
-            		game = TicLogic::makeGame();
+    	else if (gameName == "TicTacToeGame")
+            game = TicLogic::makeGame();
 
 		else if (gameName == "CardGame")
 			game = CardGame::makeGame();
